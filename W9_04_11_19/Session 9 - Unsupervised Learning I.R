fcf3de24850bb13bc79f23e7087caccf70fd19dd
prodriguezsosa <pedro.rodriguezsosa@gmail.com>
2019-04-11 12:15:37 -0400

    Update Session 9 - Unsupervised Learning I.R

--- W9_04_11_19/Session 9 - Unsupervised Learning I.R
+++ W9_04_11_19/Session 9 - Unsupervised Learning I.R
@@ -1,12 +1,12 @@
-# TA: Leslie Huang
+# TA: Pedro L. Rodríguez
 # Course: Text as Data
-# Date: 3/29/2018
-# Recitation 9: Unsupervised Learning I
+# Date: 04/11/2019
+# Lab adapted from: Leslie Huang.
 
 # Set up workspace
 rm(list = ls())
 
-setwd("/Users/lesliehuang/Text-as-Data-Lab-Spr2018/W9_03_29_18/")
+setwd("~/Drobox/GitHub/Text-as-Data-Lab-Spring-2019/W9_04_11_19/")
 
 # Loading packages
 #install.packages("lsa")
@@ -14,48 +14,86 @@ setwd("/Users/lesliehuang/Text-as-Data-Lab-Spr2018/W9_03_29_18/")
 
 library(quanteda)
 library(quanteda.corpora)
+library(dplyr)
 
 ## 1 PCA
 
-# 1.1 Two functions in base R:
+# 1.1 Two functions in base R for PCA:
+# see: http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/118-principal-component-analysis-in-r-prcomp-vs-princomp/
+?prcomp # uses the singular value decomposition approach: examines the covariances/correlations between individuals
+?princomp # uses the spectral decomposition approach: examines the covariances/correlations between variables (need more individuals than variables)
 
-?prcomp # SVD on the (centered) input data
-?princomp # eigendecomposition on the covariance matrix of the input data -- can also use option for covariance matrix 
-
-# Remember to center your data! -- use scale() on your matrix beforehand, or the option in prcomp()
+# Remember to center your data! (default = TRUE) -- use scale() on your matrix beforehand, or the option in prcomp()
 # And don't have any missing values!
 
-library(factoextra) # makes it easy to work with PCA
+library(factoextra) # makes it easy to work with PCA (great for visualization)
 
 # 1.2 Example
 data("data_corpus_sotu")
+SOTU <- corpus_subset(data_corpus_sotu, Date > "1900-01-01")
 
-SOTU_dfm <- dfm(data_corpus_sotu[145:223,], 
+SOTU_dfm <- dfm(SOTU, 
                 stem = T, 
                 remove_punct = T, 
                 remove = stopwords("english")
 )
 
+
 SOTU_mat <- convert(SOTU_dfm, to = "matrix") # convert to matrix
 
+# run pca
 SOTU_pca <- prcomp(SOTU_mat, center = TRUE, scale = TRUE)
 
-# Elbow plot
-plot(SOTU_pca, type = "l")
-
-# How much variance do the first few PCs account for?
-summary(SOTU_pca)
-
-# Eigenvalues
-head(get_eigenvalue(SOTU_pca))
-
-fviz_eig(SOTU_pca, addlabels = TRUE, ylim = c(0, 50))
+# visualize eigenvalues (scree plot: shows percentage of variance explained by each dimension)
+fviz_eig(SOTU_pca, addlabels = TRUE)
 
 # Loadings for each variable: columns contain the eigenvectors
 SOTU_pca$rotation[1:10, 1:5]
+dim(SOTU_pca$rotation)
+
+# top loadings on PC1
+pc_loadings <- SOTU_pca$rotation
+
+# token loadings
+N <- 10
+pc1_loading <- tibble(token = rownames(pc_loadings), loading = as.vector(pc_loadings[,1])) %>% arrange(-loading)
+pc1_loading$loading <- scale(pc1_loading$loading, center = TRUE)
+pc1_loading <- rbind(top_n(pc1_loading, N, loading),top_n(pc1_loading, -N, loading))
+pc1_loading <- transform(pc1_loading, token = factor(token, levels = unique(token)))
+
+# plot top tokens according to absolute loading values
+ggplot(pc1_loading, aes(token, loading)) + 
+  geom_bar(stat = "identity", fill = ifelse(pc1_loading$loading <= 0, "grey20", "grey70")) +
+  coord_flip() + 
+  xlab("Tokens") + ylab("Tokens with Top Loadings on PC1") +
+  scale_colour_grey(start = .3, end = .7) +
+  theme(panel.background = element_blank(),
+        axis.text.x = element_text(size=16),
+        axis.text.y = element_text(size=16),
+        axis.title.y = element_text(size=18, margin = margin(t = 0, r = 15, b = 0, l = 15)),
+        axis.title.x = element_text(size=18, margin = margin(t = 15, r = 0, b = 15, l = 0)),
+        legend.text=element_text(size=16),
+        legend.title=element_blank(),
+        legend.key=element_blank(),
+        legend.position = "top",
+        legend.spacing.x = unit(0.25, 'cm'),
+        plot.margin=unit(c(1,1,0,0),"cm"))
 
 # Value of the rotated data: your "new", dimensionality reduced data
 View(SOTU_pca$x)
+
+# retrieve most similar documents
+library(text2vec)
+
+# function computes cosine similarity between query and all documents and returns N most similar
+nearest_neighbors <- function(query, low_dim_space, N = 5, norm = "l2"){
+  cos_sim <- sim2(x = low_dim_space, y = low_dim_space[query, , drop = FALSE], method = "cosine", norm = norm)
+  nn <- cos_sim <- cos_sim[order(-cos_sim),]
+  return(names(nn)[2:(N + 1)])  # query is always the nearest neighbor hence dropped
+}
+
+# apply to document retrieval
+nearest_neighbors(query = "Obama-2009", low_dim_space = SOTU_pca$x, N = 5, norm = "l2")
 
 # Visualization resources:
 
@@ -69,47 +107,37 @@ View(SOTU_pca$x)
 library(lsa)
 
 # Let's keep using the SOTU data from before
+SOTU_mat_lsa <- convert(SOTU_dfm, to = "lsa") # convert to transposed matrix
+SOTU_mat_lsa <- lw_logtf(SOTU_mat_lsa) * gw_idf(SOTU_mat_lsa)
 
 # 2.1 Create LSA weights using TDM
-SOTU_lsa_auto <- lsa(t(SOTU_dfm))
-
-# Note: We are *not* doing the local/global weighting in this example!
-?gw_idf
-# From lecture
-# Local weight function: log(tf_ij + 1)
-# global weight function: 1 + sum( (p_ij * log(p_ij) ) / log(n) ) where p_ij = tf_ij/gf_i
-
+SOTU_lsa <- lsa(SOTU_mat_lsa)
+#lsa(myMatrix, dims = dimcalc_share(share = 0.8)) # share = fraction of the sum of the selected singular values over the sum of all singular values, default is 0.5
 
 # 2.2 Check to see what a good number of dimensions is
 ?dimcalc_share
 
-# lsa_obj$tk -- truncated matrix Tk from term vector matrix T (constituting left singular vectors from the SVD of the original matrix)
-# svd(matrix)$d -- singular values of svd(matrix)
-SOTU_lsa_auto_svd <- svd(SOTU_lsa_auto$tk)$d
-
-dimcalc_share(share = 0.5)(SOTU_lsa_auto_svd)
-
-
-# By default, share is set to .5; let's try .9
-# share = fraction of the sum of the selected singular values over the sum of all singular values
-dimcalc_share(share = 0.9)(SOTU_lsa_auto_svd)
+# lsa_obj$tk = truncated term matrix from term vector matrix T (constituting left singular vectors from the SVD of the original matrix)
+# lsa_obj$dk = truncated document matrixfrom document vector matrix D (constituting right singular vectors from the SVD of the original matrix)
+# lsa_obj$sk = singular values
+# see: https://cran.r-project.org/web/packages/lsa/lsa.pdf
 
 # Lecture example uses dims = 5
-SOTU_lsa_5 <- lsa(t(SOTU_dfm), 5 )
+SOTU_lsa_5 <- lsa(SOTU_mat_lsa, 5)
 
-SOTU_lsa_5_mat <- t(as.textmatrix(SOTU_lsa_5) )
-
+# display generated LSA space
+?as.textmatrix
+SOTU_lsa_5_mat <- t(as.textmatrix(SOTU_lsa_5))
 
 # 2.3 Compare features for a few speeches
-
 SOTU_dfm@Dimnames$docs[9]
 topfeatures(SOTU_dfm[9,])
 
 # With 5 dims:
 sort(SOTU_lsa_5_mat[9,], decreasing=T)[1:10]
 
-# With auto (14) dims:
-sort(t(as.textmatrix(SOTU_lsa_auto))[9, ], decreasing = T)[1:10]
+# With auto (42) dims:
+sort(t(as.textmatrix(SOTU_lsa))[9, ], decreasing = T)[1:10]
 
 # Another example:
 SOTU_dfm@Dimnames$docs[55]
@@ -122,37 +150,26 @@ sort(t(as.textmatrix(SOTU_lsa_auto))[55, ], decreasing = T)[1:10]
 # associate(): a method to identify words that are most similar to other words using a LSA
 ?associate
 # uses cosine similarity between input term and other terms
+SOTU_lsa_mat <- as.textmatrix(SOTU_lsa) 
 
-SOTU_lsa_3 <- lsa(t(SOTU_dfm), 3 )
-
-SOTU_lsa_3_mat <- as.textmatrix(SOTU_lsa_3) 
-
-china <- associate(SOTU_lsa_3_mat, "china", "cosine", threshold = .7)
-china[1:10]
-
-
-oil <- associate(SOTU_lsa_3_mat, "oil", "cosine", threshold = .7)
+oil <- associate(SOTU_lsa_mat, "oil", "cosine", threshold = .7)
 oil[1:10]
 
-america <- associate(SOTU_lsa_3_mat, "america", "cosine", threshold = .7)
-america[1:10]
-
-health <- associate(SOTU_lsa_3_mat, "health", "cosine", threshold = .7)
+health <- associate(SOTU_lsa_mat, "health", "cosine", threshold = .7)
 health[1:10]
 
 # Keep this in mind when we do topic models!
-
 
 ## 2 WORDFISH
 
 # How is it different from other approaches we've used for scaling?
 
 # 2.1 Read in conservative and labour manifestos (from Recitation 6)
-setwd("../W6_02_27_18/cons_labour_manifestos")
+setwd("~/Drobox/GitHub/Text-as-Data-Lab-Spring-2019/W6_03_07_19/cons_labour_manifestos")
 
 files <- list.files( full.names=TRUE)
 text <- lapply(files, readLines)
-text<-unlist(lapply(text, function(x) paste(x, collapse = " ")))
+text <- unlist(lapply(text, function(x) paste(x, collapse = " ")))
 
 # Name data
 files <- unlist(files)
@@ -168,15 +185,15 @@ party <- party[party!="a" & party!="b"]
 
 #create data frame
 man_df <- data.frame(year = factor(as.numeric(year)),
-                   party = factor(party),
-                   text = text,
-                   stringsAsFactors = FALSE)
+                     party = factor(party),
+                     text = text,
+                     stringsAsFactors = FALSE)
 
 lab_con_dfm <- dfm(man_df$text, 
                    stem = T, 
                    remove = stopwords("english"), 
                    remove_punct = T
-                   )
+)
 
 # 2.2 fit wordfish
 
@@ -194,7 +211,6 @@ words <- manifestos_fish$psi # values
 names(words) <- manifestos_fish$features # the words
 
 sort(words)[1:50]
-
 sort(words, decreasing=T)[1:50]
 
 # Guitar plot
